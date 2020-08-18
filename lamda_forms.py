8aa9f83e61bef64bbcca6fa98a0279b57ca76515
Aanjul <32233726+Aanjul@users.noreply.github.com>
2020-08-18 18:56:36 +0530

    lamda_forms_speed_test.py

---
+++ lamda_forms.py
@@ -0,0 +1,42 @@
+# For refrence http://www.hlevkin.com/Shell_progr/hellopython.htm
+
+#! python
+
+#Lamda Forms
+
+def make_incrementor(n):
+           return lamda x: x + n
+
+#=========================================
+
+f = make_incrementor(42)
+
+print f(10)
+print f(1)
+print f(15)
+
+//===============================================
+//
+//===============================================
+
+#! python
+
+#speed test
+nn=1000000
+i=0;
+s=0;
+
+print "beginning..."
+
+while i
+#! python
+
+# raw input pf strings only!
+
+st = raw_input("")
+
+print st
+
+st=st*3 # triple the string
+
+print st
