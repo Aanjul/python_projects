37d87d39fe52b9b1b9cb5cf4024b436b9924be8c
Aanjul <32233726+Aanjul@users.noreply.github.com>
2020-08-27 19:53:22 +0530

    script to check the current environment of system

---
+++ script_to_check_the_environment.py
@@ -0,0 +1,56 @@
+# For refrence https://github.com/realpython/python-scripts/blob/master/scripts/15_check_my_environment.py
+
+
+
+"""
+
+Pass in a config file based on your environment.
+
+Example:
+
+import check_my_environment
+
+
+
+class Main:
+     def__init__(self, configFile):
+     pass
+
+    def process(self):
+        print("ok")
+
+ if__name__=="__main__":
+      m = Main(some_script.CONFIGFILE)
+      m.process() 
+
+   """
+
+
+
+
+
+   import os
+   import sys
+   ENVIRONMENT = "development"
+   CONFIGFILE = None
+
+
+   def get_config_file():
+       directoy = os.path.dirname(__file__)
+       return {
+           "development": "{}/../config/development.cfg".format(directory),
+           "staging":"{}/../config/staging.cfg".format(directory),
+           "production": "{}/../config/production.cfg".format(directory)
+       }.get(ENVIRONMENT, None)
+
+       CONFIGFILE = get_config_file()
+
+       if CONFIGFILE is None:
+           sys.exit("Configuration error! Unknown environment set. \
+               Edit config.py and set appropriate environment")
+        print("Config file: {}".format(CONFIGFILE))
+        if not os.path.exists(CONFIGFILE):
+            sys.exit("Configuration error! Config file does not exist")
+            print("Config ok...")       
+
+
