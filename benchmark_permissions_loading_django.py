05fc0d272424ce01d6f439cc2a1b01ad29285eff
Aanjul <sharmaaanjul5@gmail.com>
2020-09-18 20:35:36 +0530

    script for benchmark loading of permissions in Django

---
+++ benchmark_permissions_loading_django.py
@@ -0,0 +1,43 @@
+# For refrence https://github.com/realpython/python-scripts/blob/master/scripts/07_benchmark_permissions_loading_django.py 
+
+import os
+import time
+import numpy
+
+#temp file for benchmarking
+
+
+def timeit(method):
+
+      def timed(*args, **kw):
+      ts = time.time()
+
+      result = method(*args, **kw)
+      te = time.time()
+      all_times.append(te - ts)
+
+      print(all_times)
+      print(numpy.mean(all_times))
+      return result
+
+      return timed
+
+def create_new_db():
+    os.system("mysqladmin -u root drop DATABASE_NAME -f")
+    os.system("mysqladmin -u root create DATABASE_NAME -f")
+    os.system("./manage.py syncdb")
+    os.system("./manage.py migrate")
+
+
+@timeit
+def load_new_perms():
+    os.system("./manage.py LOAD_PERMS_COMMAND")
+
+
+    if __name__ == "__main__":
+        n = 0
+        all_times = list ()
+        while n < 10:
+            create_new_db()
+            load_new_perms()
+            n += 1
