50a360e4eafb5fecd72da5553017afe25f3ccc26
Aanjul <sharmaaanjul5@gmail.com>
2020-12-16 21:30:19 +0530

    script for Twitter Bot

---
+++ script_for_tweeter_bot.py
@@ -0,0 +1,29 @@
+#For refrence https://github.com/realpython/python-scripts/blob/master/scripts/21_twitter_bot.py
+
+import tweepy
+
+# Authentication credentials - dev.twitter.com
+
+cfg = {
+    'consumer_key': 'VALUE',
+    'consumer_secret': 'VALUE',
+    'access_token': 'VALUE',
+    'access_token_secret': 'VALUE' 
+}
+
+
+def get_api_handler(cfg):
+    auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+    auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+    return tweepy.API(auth)
+
+
+
+    def main():
+        api = get_api_handler(cfg)
+        tweet = 'Gracias, my friend you have got the access!'
+        api.update_status(status=tweet)
+
+
+        if__name__=="__main__":
+        main()
