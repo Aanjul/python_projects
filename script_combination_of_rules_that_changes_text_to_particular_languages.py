dddd8015a7f8c958e4f36bf80db2ae0e7c42160b
Aanjul <32233726+Aanjul@users.noreply.github.com>
2021-06-05 18:35:50 +0530

    pig latin translator that encrypts the text
    pig Latin refers to a combination of rules that changes the text in a particular language to make it difficult to understand for someone who is not trained.

---
+++ script_combination_of_rules_that_changes_text_to_particular_languages.py
@@ -0,0 +1,24 @@
+# For refrence https://www.fosslinux.com/46256/python-script-examples.htm
+
+#latin_translator.py
+
+#request user for input
+user_input = input("Input text translate to pig latin:")
+print("User Text:", user_input)
+
+
+#This step breaks the words into a list
+updated_user_input = user_input.split('')
+
+for j  in updated_user_input:
+    if len(j) >= 3: #only translate words with more than 3 characters
+    j = j + "%say" % (j[0])
+    j = j[1:]
+    print(j)
+else:
+    pass
+
+
+# To execute latin_translator.py from the terminal, type the following code.
+# python3 latin_translator.py
+
