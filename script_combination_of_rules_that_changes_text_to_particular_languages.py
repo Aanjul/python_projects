ca205f64731bc2081aed8dd6214df3853f996c0c
Aanjul <32233726+Aanjul@users.noreply.github.com>
2021-06-08 02:24:50 +0530

    space removed bugs fixed

--- script_combination_of_rules_that_changes_text_to_particular_languages.py
+++ script_combination_of_rules_that_changes_text_to_particular_languages.py
@@ -3,20 +3,21 @@
 #latin_translator.py
 
 #request user for input
-user_input = input("Input text translate to pig latin:")
-print("User Text:", user_input)
-
+user_input = input("Input text to translate to pig latin: ")
+print("User Text: ", user_input)
 
 #This step breaks the words into a list
-updated_user_input = user_input.split('')
+updated_user_input = user_input.split(' ')
 
-for j  in updated_user_input:
-    if len(j) >= 3: #only translate words with more than 3 characters
-    j = j + "%say" % (j[0])
-    j = j[1:]
-    print(j)
+for j in updated_user_input:
+ if len(j) >= 3: #only translate words with more than 3 characters
+  j = j + "%say" % (j[0]) 
+  j = j[1:]
+  print(j)
 else:
     pass
+
+
 
 
 # To execute latin_translator.py from the terminal, type the following code.
