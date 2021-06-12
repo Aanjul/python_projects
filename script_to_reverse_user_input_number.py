599c758e89781d9178962193b8808db62e19a9da
Aanjul <32233726+Aanjul@users.noreply.github.com>
2021-06-12 19:12:33 +0530

    script takes user input number and reverse it

---
+++ script_to_reverse_user_input_number.py
@@ -0,0 +1,16 @@
+# For reference https://www.fosslinux.com/46256/python-script-examples.htm
+
+
+#reverse_number_number.py
+
+
+user_input=int(input("Enter the number to reverse: "))
+_rev=0
+while(user_input>0):
+    dig=user_input%10
+    _rev=_rev*10+dig
+    user_input=user_input//10
+    print("The reversed number is :",_rev)
+
+    #Command to execute the script over terminal
+    
