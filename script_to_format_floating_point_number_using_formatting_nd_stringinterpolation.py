fa510dff918fca3183e92532e4620588e28cc28c
Aanjul <32233726+Aanjul@users.noreply.github.com>
2021-06-13 18:59:41 +0530

    formats stringByStringFormatting&Interpolation
    script formats a floating-point number using string formatting and string interpolation

---
+++ script_to_format_floating_point_number_using_formatting_nd_stringinterpolation.py
@@ -0,0 +1,15 @@
+#For Reference https://www.fosslinux.com/46256/python-script-examples.htm
+
+
+#application of String Formatting
+first_val = 365.48951
+print("String Formatting: {:5.2f}".format(first_val))
+
+#application of String Interpolation
+second_val= 365.48951
+print("String Interpolation: %5.2f" % second_val)
+
+
+#terminal command to execute python3 floating_point_number.py
+
+
