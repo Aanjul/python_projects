ec57929b247357c12001ec1240c98ca4b937afcd
Aanjul <32233726+Aanjul@users.noreply.github.com>
2021-08-03 14:37:35 +0530

    script_to_reduce_image_size_image
    This script compresses the image choosen to much reduced size. Image can be of any format. Note : The image should be in same folder as the script It will return with the compressed image when you compile. It automates the task of compressing the image in day to day lives.

---
+++ script_to_reduce_image_size.py
@@ -0,0 +1,15 @@
+# For refrence https://github.com/hastagAB/Awesome-Python-Scripts/tree/master/Image_Compressor
+
+
+import PIL
+from PIL import Image
+from tkinter.filedialog import *
+
+file_path=askopenfilenames()
+img = PIL.Image.open(file_path)
+myHeight,myWidth = img.size
+
+img=img.resize((myHeight,myWidth),PIL.Image.ANTILIAS)
+save_path=asksaveasfile()
+
+img.save(save_path+"_compressed.JPG")
