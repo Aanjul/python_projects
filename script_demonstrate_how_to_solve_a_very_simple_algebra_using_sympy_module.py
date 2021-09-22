1d10d48ace543c3688f9571cd6b55b03c26a6a0c
Aanjul <32233726+Aanjul@users.noreply.github.com>
2021-09-22 12:12:54 +0530

    ScriptDemonstratesHowToSolveSimpleAlgebraBysympy
    Python Algebra Solver
    This python script will demonstrate how to solve a very simple algebra using sympy module
    
    Requirement
    Python 3.xx or Python 2.xx Sympy
    
    pip install sympy
    #Usage Call python following with the simple algebra problem
    
    $ python Solver.py "5 = X - 2"

---
+++ script_demonstrate_how_to_solve_a_very_simple_algebra_using_sympy_module.py
@@ -0,0 +1,19 @@
+#https://github.com/hastagAB/Awesome-Python-Scripts/blob/master/Algebra-Solver/Algebra-Solver.py
+
+#!/usr/bin/python3
+
+from sympy import *
+import sys
+
+def solver(x):
+	    sympy_eq = sympify("Eq(" + x.replace("=", ",") + ")")
+		result = solve(sympy_eq)
+		return result[0]
+		
+def main(x):
+		X = solver(x)
+		print("X = " + str(X))
+		
+if __name__ == "__main__":
+	main(sys.argv[1])
+	
