d0aae5e5ca5f7825c73dae743d200c577672239b
Aanjul <32233726+Aanjul@users.noreply.github.com>
2021-09-27 12:59:07 +0530

    script extracts text from PDF file
    Description: PDF2text
    this is a small script to make a extract text from pdf file.
    
    Dependencies:
    1- pdftotext
    
    Usage
    Run python script.py then enter path of pdf file.

---
+++ script_extracts_text_from_pdf_file.py
@@ -0,0 +1,17 @@
+# For refrence https://github.com/hastagAB/Awesome-Python-Scripts/tree/master/PDF2text
+
+import os
+import pdftotext
+
+
+pdf_path = input("Enter the path of the pdf file : ")
+
+assert os.path.exists(pdf_path), "this pdf file doesn't exist"
+
+with open(pdf_path, 'rb') as f_r:
+	pdf_pages = pdftotext.PDF(f_r)
+	
+for i, page in enumerate(pdf_pages):
+	print('Page {}'.format(i))
+	print(page)
+	print('*'*100)
