79e467d9b80c3e40dcb5e02b244c9e7e0dec19c5
Aanjul <32233726+Aanjul@users.noreply.github.com>
2021-12-03 21:31:46 +0530

    script prints a calendar specified monthAndYear
    A program to print a calendar of the specified month and year
    Python has a built-in module, the calendar, which helps to access a record of the calendar. There are many methods in this module, in this program, we are trying to print the calendar of the specified month of a given year.

---
+++ script_program_to_print_a_calendar_of_specified_month_and_year.py
@@ -0,0 +1,7 @@
+# For refrence https://levelup.gitconnected.com/10-interesting-python-programs-with-code-b676181a2d1a
+
+
+import calendar
+year =int( input("Enter the year of the required calendar "))
+month = int( input("Enter the month of the required calendar "))
+print(calendar.month(year,month))
