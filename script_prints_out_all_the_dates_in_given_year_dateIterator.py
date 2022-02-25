38a42771d081b7cc6cef065067f0ef0d2af56701
Aanjul <32233726+Aanjul@users.noreply.github.com>
2022-02-25 13:12:53 +0530

    script printsOutAll DatesInGivenYear DateIteratoor
    https://github.com/fnplus/Python-scripts-collection
    
    DateIterator
    	A simple script that prints out all the dates in a given year.

---
+++ script_prints_out_all_the_dates_in_given_year_dateIterator.py
@@ -0,0 +1,21 @@
+# For refrence https://github.com/fnplus/Python-scripts-collection/blob/master/DateIterator/Date_iterator.py
+
+#https://github.com/fnplus/Python-scripts-collection
+
+from datetime import datetime, timedelta
+
+
+user_input = input("pick a year ")
+
+while not user_input.isdigit():
+ print("Please enter a year.")
+ user_input = input("pick a year ")
+
+currently_read = start_datetime = datetime(int(user_input), 1, 1, 0, 0, 0)
+end_datetime = datetime(int(user_input), 12, 31, 0, 0, 0)
+difference = end_datetime - start_datetime
+
+while currently_read < end_datetime:
+	print((currently_read + timedelta(days=1)).strftime("%Y-%m-%d"),)
+	
+	currently_read += timedelta(days=1)
