9e5ff465c78b06463a6994fb325ff955e9c8ecf0
Aanjul <32233726+Aanjul@users.noreply.github.com>
2022-03-11 14:20:38 +0530

    scriptForGivenDirectoryLocationFindsSize&PathOfAllFiles
    https://github.com/fnplus/Python-scripts-collection
    
    For given directory location finds the size and path of all files of a particular extension present in the directory (including sub-directories).

---
+++ script_for_given_directory_location_finds_the_size_&_path_of_all_files_for_particular_extension_present_in_directory_including_subdirectories.py
@@ -0,0 +1,50 @@
+# For refrence https://github.com/fnplus/Python-scripts-collection/blob/master/FileLocator/code.py
+
+
+import os
+from os.path import join
+
+def get_human_size(file_size):
+	"""
+	Provides a human-readable size from the
+	given file size (in bytes).
+	:param int file_size:
+		This file size to convert.
+	:rtype: str
+	"""
+	for human_size_description in [
+		"bib",
+		"KiB",
+		"MiB",
+		"GiB",
+		"TiB",
+		"PiB",
+		"EiB",
+		"ZiB",
+		"YiB",
+		]:
+			if file_size < 1024:
+				human_size = f"{file_size:3.1f}{human_size_description}"
+				break
+				
+			file_size /= 1024.0
+		
+		return human_size
+if __name__ == "__main__":
+	search_location = str(input("Directory Location : ")).strip()
+	extension = str(input("Extension of files to search for : ")).strip()
+	
+	if extension.startswith("."):
+		extension = extension[1:]
+	
+	for dirname, _, files in os.walk(search_location):
+		for filename in files:
+			if filename.endswith(f".{extension}"):
+				the_file = os.path.join(dirname, filename)
+				
+				print(
+					get_human_size(os.path.getsize(the_file)),
+					the_file,
+					)
+				
+			 
