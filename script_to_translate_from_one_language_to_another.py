7411d0bef90e812942bfa9f0c7dfcd069ef4f345
Aanjul <32233726+Aanjul@users.noreply.github.com>
2022-06-02 20:56:47 +0530

    script toTranslateOne Language to Another Language
    Translation Script
    Running this Script would translate one language to another language
    
    Dependency
    pip install googletrans==3.1.0a0
    
    Working
    It works with the help of Google Trans Library
    Run this command on a python interpreter python Translation.py
    Enter what you want to translate
    Enter the Code of Language
    Make sure to give correct language code in the script
    Boom!! Here is the Translation
    Documentation for language Codes
    https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
    
    Example
    python Translation.py
    
    OUT: धन्यवाद

---
+++ script_to_translate_from_one_language_to_another.py
@@ -0,0 +1,14 @@
+# For refrence https://github.com/avinashkranjan/Amazing-Python-Scripts/blob/master/Translator%20Script/Translation.py
+
+# Using the Translator Class
+from googletrans import Translator
+# Making Object of Translator Class
+translator = Translator()
+# Word you want to translate
+Word = input('Enter what you want to translate : ')
+# For further language Codes
+# https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
+Code = input('Enter Language Code : ')
+# Using the translator method to get work done
+translation = translator.translate(Word, dest=Code)
+print(translation.text)
